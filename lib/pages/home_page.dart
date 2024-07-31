11c76528a8200096f3af7106a819c1ea03b61152
KeingLCasabona <keing.limache.casabona@gmail.com>
2024-07-30 23:41:33 -0500

    feat: Add refactory

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -41,9 +41,42 @@ class HomePage extends StatelessWidget {
             SizedBox(
               width: 160,
               child: Divider(),
-            )
+            ),
+            ItemComponentWidget(),
+            ItemComponentWidget(),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class ItemComponentWidget extends StatelessWidget {
+  const ItemComponentWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(14),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.5),
+              blurRadius: 10,
+              offset: Offset(4, 4),
+            )
+          ]),
+      child: ListTile(
+        leading: Icon(
+          Icons.check_circle_outline,
+          color: Color.fromARGB(255, 12, 130, 44),
+        ),
+        title: Text('Avatar', style: GoogleFonts.poppins()),
+        subtitle:
+            Text('Ir al detalle del avatar', style: GoogleFonts.poppins()),
+        trailing: Icon(Icons.chevron_right, color: Colors.blue),
       ),
     );
   }
