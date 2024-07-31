17d45d60451d1af42c9e9f45fc288fdf35f2e0c0
KeingLCasabona <keing.limache.casabona@gmail.com>
2024-07-31 15:20:25 -0500

    feat: Add Avatar complete

--- lib/pages/avatar_page.dart
+++ lib/pages/avatar_page.dart
@@ -7,7 +7,37 @@ class AvatarPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Avatar Page'),
+        title: Text(
+          'Avatar Page',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.deepPurple,
+        iconTheme: IconThemeData(color: Colors.white),
+        actions: [
+          CircleAvatar(
+            backgroundImage: NetworkImage(
+                'https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.more_vert),
+          )
+        ],
+      ),
+      body: Column(
+        children: [
+          FadeInImage(
+            image: NetworkImage(
+                'https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
+            placeholder: AssetImage('assets/gif/loading.gif'),
+            fit: BoxFit.cover,
+            fadeInDuration: Duration(milliseconds: 1300),
+          ),
+
+          // Image.network(
+          //     'https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
+          // Image.asset('assets/gif/loading.gif'),
+        ],
       ),
     );
   }
