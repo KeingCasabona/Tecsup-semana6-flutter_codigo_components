e24f872205af98dd4d5b15f33ba8847e3f01b857
KeingLCasabona <keing.limache.casabona@gmail.com>
2024-08-01 11:27:00 -0500

    feat: Add alert dialog

--- lib/pages/alert_page.dart
+++ lib/pages/alert_page.dart
@@ -3,11 +3,54 @@ import 'package:flutter/material.dart';
 class AlertPage extends StatelessWidget {
   const AlertPage({super.key});
 
+  showMyAlert(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+            title: Text('Titulo del Alert'),
+            backgroundColor: Colors.white,
+            content: Text(
+                'Este es el contenido de mi alert, este es un texto de prueba'),
+            actions: [
+              TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text('Cancelar')),
+              TextButton(onPressed: () {}, child: Text('Aceptar')),
+            ]);
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Alert Page'),
+        title: Text(
+          'Alert Page',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        backgroundColor: Colors.black,
+        iconTheme: IconThemeData(
+          color: Colors.white,
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              child: Text('Hola'),
+              onPressed: () {
+                showMyAlert(context);
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
