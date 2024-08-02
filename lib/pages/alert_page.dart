ec2ada53682b266f0504b30140d3e57142d4e82e
KeingLCasabona <keing.limache.casabona@gmail.com>
2024-08-02 00:09:36 -0500

    feat: Add alert dialog2

--- lib/pages/alert_page.dart
+++ lib/pages/alert_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AlertPage extends StatelessWidget {
   const AlertPage({super.key});
@@ -24,6 +26,75 @@ class AlertPage extends StatelessWidget {
     );
   }
 
+  showMyAlert2(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          contentPadding: EdgeInsets.all(20),
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+          content: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                'Hola a todos',
+                style: GoogleFonts.poppins(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black87,
+                  fontSize: 16,
+                ),
+              ),
+              Divider(thickness: 0.7),
+              CircleAvatar(
+                backgroundColor: Colors.black54,
+                backgroundImage: NetworkImage(
+                    'https://images.pexels.com/photos/428364/pexels-photo-428364.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.star, color: Colors.yellow),
+                  Icon(Icons.star, color: Colors.yellow),
+                  Icon(Icons.star, color: Colors.yellow),
+                  Icon(Icons.star, color: Colors.yellow),
+                  Icon(Icons.star, color: Colors.yellow),
+                ],
+              ),
+              SizedBox(height: 10),
+              Text(
+                'Este es el contenido y vamos a seguir practicando como siempre',
+                textAlign: TextAlign.center,
+                style: GoogleFonts.poppins(
+                  color: Colors.black54,
+                  fontSize: 13,
+                ),
+              ),
+              SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text('Cancelar'),
+                  ),
+                  SizedBox(width: 8),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text('Aceptar'),
+                  ),
+                ],
+              )
+            ],
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,11 +115,17 @@ class AlertPage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             ElevatedButton(
-              child: Text('Hola'),
+              child: Text('Alert1'),
               onPressed: () {
                 showMyAlert(context);
               },
             ),
+            ElevatedButton(
+              child: Text('Alert2'),
+              onPressed: () {
+                showMyAlert2(context);
+              },
+            )
           ],
         ),
       ),
