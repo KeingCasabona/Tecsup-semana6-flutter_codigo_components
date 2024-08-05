f9c0307919344256f6518d91c58f4ad989b5242f
KeingLCasabona <keing.limache.casabona@gmail.com>
2024-08-04 19:04:54 -0500

    feat: Add cards with containers 3

--- lib/pages/card_page.dart
+++ lib/pages/card_page.dart
@@ -1,4 +1,8 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 
@@ -115,6 +119,54 @@ class CardPage extends StatelessWidget {
                     ],
                   ),
                 )
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(20),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.5),
+                  blurRadius: 12,
+                  offset: Offset(4, 4),
+                ),
+              ],
+            ),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Text(
+                      maxLines: 7,
+                      overflow: TextOverflow.ellipsis,
+                      'Ea tempor est occaecat reprehenderit reprehenderit ut veniam anim cillum voluptate magna amet. Ea sit do nulla ex qui elit ad aliquip do eu aliqua cillum est esse. Mollit nostrud nisi reprehenderit sit dolore pariatur elit fugiat officia occaecat excepteur duis laboris. Anim consectetur aute id anim id duis exercitation qui exercitation. Quis ut pariatur consequat velit amet consequat sit. Ipsum labore ut ut ut non irure dolore voluptate. Amet irure enim est tempor nisi velit.',
+                      style: TextStyle(),
+                    ),
+                  ),
+                ),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(14),
+                  child: Image.network(
+                    'https://i.pinimg.com/736x/ab/f2/85/abf2850ac9d31466c25f945055073840.jpg',
+                    height: 160,
+                    width: 90,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                // Container(
+                //   width: 80,
+                //   height: 160,
+                //   decoration: BoxDecoration(
+                //       borderRadius: BorderRadius.circular(20),
+                //       image: DecorationImage(
+                //           fit: BoxFit.cover,
+                //           image: NetworkImage(
+                //               'https://i.pinimg.com/736x/ab/f2/85/abf2850ac9d31466c25f945055073840.jpg'))),
+                // ),
               ],
             ),
           )
