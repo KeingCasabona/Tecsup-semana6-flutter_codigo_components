751195194f2efa40684c5569d4a20ea7ff2224c1
KeingCasabona <keing.limache.casabona@gmail.com>
2024-08-23 19:25:14 -0500

    feat: Add GridView with hero

--- lib/pages/grid_page.dart
+++ lib/pages/grid_page.dart
@@ -8,6 +8,118 @@ class GridPage extends StatefulWidget {
 }
 
 class _GridPageState extends State<GridPage> {
+  Map<String, dynamic> teamMap = {
+    "squadName": "Super hero squad",
+    "homeTown": "Metro City",
+    "formed": 2016,
+    "secretBase": "Super tower",
+    "active": true,
+    "members": [
+      {
+        "name": "Batman",
+        "image":
+            "https://www.lacasadeel.net/wp-content/uploads/2021/11/BATMAN-ENCABEZADO.jpg",
+        "age": 29,
+        "secretIdentity": "Dan Jukes",
+        "powers": ["Radiation resistance", "Turning tiny", "Radiation blast"]
+      },
+      {
+        "name": "Superman",
+        "image":
+            "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/styles/980px/public/media/image/2021/06/superman-2354819.jpg",
+        "age": 39,
+        "secretIdentity": "Jane Wilson",
+        "powers": [
+          "Million tonne punch",
+          "Damage resistance",
+          "Superhuman reflexes"
+        ]
+      },
+      {
+        "name": "Wonder Woman",
+        "image":
+            "https://dam.smashmexico.com.mx/wp-content/uploads/2021/10/wonder-woman-historia-comics-escenciales-cover.jpg",
+        "age": 1000000,
+        "secretIdentity": "Unknown",
+        "powers": [
+          "Immortality",
+          "Heat Immunity",
+          "Inferno",
+          "Teleportation",
+          "Interdimensional travel"
+        ]
+      },
+      {
+        "name": "Batman",
+        "image":
+            "https://www.lacasadeel.net/wp-content/uploads/2021/11/BATMAN-ENCABEZADO.jpg",
+        "age": 29,
+        "secretIdentity": "Dan Jukes",
+        "powers": ["Radiation resistance", "Turning tiny", "Radiation blast"]
+      },
+      {
+        "name": "Superman",
+        "image":
+            "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/styles/980px/public/media/image/2021/06/superman-2354819.jpg",
+        "age": 39,
+        "secretIdentity": "Jane Wilson",
+        "powers": [
+          "Million tonne punch",
+          "Damage resistance",
+          "Superhuman reflexes"
+        ]
+      },
+      {
+        "name": "Wonder Woman",
+        "image":
+            "https://dam.smashmexico.com.mx/wp-content/uploads/2021/10/wonder-woman-historia-comics-escenciales-cover.jpg",
+        "age": 1000000,
+        "secretIdentity": "Unknown",
+        "powers": [
+          "Immortality",
+          "Heat Immunity",
+          "Inferno",
+          "Teleportation",
+          "Interdimensional travel"
+        ]
+      },
+      {
+        "name": "Batman",
+        "image":
+            "https://www.lacasadeel.net/wp-content/uploads/2021/11/BATMAN-ENCABEZADO.jpg",
+        "age": 29,
+        "secretIdentity": "Dan Jukes",
+        "powers": ["Radiation resistance", "Turning tiny", "Radiation blast"]
+      },
+      {
+        "name": "Superman",
+        "image":
+            "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/styles/980px/public/media/image/2021/06/superman-2354819.jpg",
+        "age": 39,
+        "secretIdentity": "Jane Wilson",
+        "powers": [
+          "Million tonne punch",
+          "Damage resistance",
+          "Superhuman reflexes"
+        ]
+      },
+      {
+        "name": "Wonder Woman",
+        "image":
+            "https://dam.smashmexico.com.mx/wp-content/uploads/2021/10/wonder-woman-historia-comics-escenciales-cover.jpg",
+        "age": 1000000,
+        "secretIdentity": "Unknown",
+        "powers": [
+          "Immortality",
+          "Heat Immunity",
+          "Inferno",
+          "Teleportation",
+          "Interdimensional travel"
+        ]
+      },
+    ]
+  };
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,33 +132,28 @@ class _GridPageState extends State<GridPage> {
         crossAxisSpacing: 10,
         mainAxisSpacing: 10,
         crossAxisCount: 2,
-        childAspectRatio: 3,
-        children: [
-          Container(color: Colors.amber),
-          Container(color: const Color.fromARGB(255, 7, 52, 255)),
-          Container(color: const Color.fromARGB(255, 255, 7, 69)),
-          Container(color: const Color.fromARGB(255, 81, 255, 7)),
-          Container(color: Colors.amber),
-          Container(color: const Color.fromARGB(255, 7, 52, 255)),
-          Container(color: const Color.fromARGB(255, 255, 7, 69)),
-          Container(color: const Color.fromARGB(255, 81, 255, 7)),
-          Container(color: Colors.amber),
-          Container(color: const Color.fromARGB(255, 7, 52, 255)),
-          Container(color: const Color.fromARGB(255, 255, 7, 69)),
-          Container(color: const Color.fromARGB(255, 81, 255, 7)),
-          Container(color: Colors.amber),
-          Container(color: const Color.fromARGB(255, 7, 52, 255)),
-          Container(color: const Color.fromARGB(255, 255, 7, 69)),
-          Container(color: const Color.fromARGB(255, 81, 255, 7)),
-          Container(color: Colors.amber),
-          Container(color: const Color.fromARGB(255, 7, 52, 255)),
-          Container(color: const Color.fromARGB(255, 255, 7, 69)),
-          Container(color: const Color.fromARGB(255, 81, 255, 7)),
-          Container(color: Colors.amber),
-          Container(color: const Color.fromARGB(255, 7, 52, 255)),
-          Container(color: const Color.fromARGB(255, 255, 7, 69)),
-          Container(color: const Color.fromARGB(255, 81, 255, 7)),
-        ],
+        childAspectRatio: 2,
+        children: teamMap['members']
+            .map<Widget>(
+              (item) => Container(
+                decoration: BoxDecoration(
+                  color: Colors.amber,
+                  image: DecorationImage(
+                    image: NetworkImage(item['image']),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                child: Text(
+                  item['name'],
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            )
+            .toList(),
       ),
     );
   }
